98f53f2b31974159015e21097ad5d88f65e21a29
christoffertinsley <ctinsley@sixthavestudios.com>
2016-04-07 11:45:05 -0700

    Update start.R

--- start.R
+++ start.R
@@ -1,5 +1,5 @@
 #Install Rserve
 install.packages('Rserve', repos='http://cran.r-project.org')
-
+library('Rserve')
 #Start Rserve
 Rserve(debug = TRUE, 6311, args = NULL, config.file = "Rserv.conf")
